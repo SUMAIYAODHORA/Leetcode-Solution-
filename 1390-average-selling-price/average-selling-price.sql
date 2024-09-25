78695917d3743932dc4a977a498bd1c4703ec57b
Sumaiya Rahman Aodhora <104058307+SUMAIYAODHORA@users.noreply.github.com>
2024-09-26 02:07:18 +0600

    Delete 1390-average-selling-price directory

--- 1390-average-selling-price/average-selling-price.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT Prices.product_id,IFNULL(ROUND(SUM(Prices.price*UnitsSold.units)/SUM(UnitsSold.units),2),0) AS average_price
-FROM Prices LEFT JOIN UnitsSold
-ON Prices.product_id = UnitsSold.product_id 
-AND purchase_date BETWEEN start_date AND end_date
-GROUP BY product_id;
